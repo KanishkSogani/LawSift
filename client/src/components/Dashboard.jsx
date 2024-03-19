e3cabd511a7986210ace413614fe45f1d3196088
KanishkSogani <kanishksogani1@gmail.com>
2024-03-20 02:40:39 +0530

    Pros and cons added

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -7,11 +7,16 @@ function Dashboard({ data }) {
   const [summary, setSummary] = useState([]);
   const [strpr, setStrpr] = useState();
   const [weakpr, setWeakpr] = useState();
+  const [pro, setPro] = useState([]);
+  const [con, setCon] = useState([]);
+
   useEffect(() => {
     if (data.length > 0) {
       setSummary(data[0].summary);
       setStrpr(data[3].Strength_Percentage);
       setWeakpr(data[4].weakness_percentage);
+      setPro(data[5].additional_sections);
+      setCon(data[6].missing_sections);
     }
   }, [data]);
   return (
@@ -40,7 +45,7 @@ function Dashboard({ data }) {
               width: "60vw",
               minHeight: "60vh",
               maxHeight: "60vh",
-              padding: 20,
+              // padding: 20,
               backgroundColor: " #CCCCFF",
               overflowY: "auto",
               className: "style-2",
@@ -48,11 +53,17 @@ function Dashboard({ data }) {
               marginRight: "2vw",
               color: "black",
               fontSize: "20px",
-              borderRadius: 20,
+              borderRadius: 5,
             }}
           >
             {summary.map((item) => (
-              <p style={{ border: "1px dotted black", padding: ".5rem" }}>
+              <p
+                style={
+                  item === ""
+                    ? null
+                    : { border: "1px solid black", padding: ".5rem" }
+                }
+              >
                 {item}
               </p>
             ))}
@@ -79,7 +90,7 @@ function Dashboard({ data }) {
                 labels: ["Strength", "Weakness"],
                 datasets: [
                   {
-                    data: [10, 20],
+                    data: [strpr, weakpr],
                     backgroundColor: [
                       "rgb(207, 159, 255)",
                       "rgb(218, 112, 214)",
@@ -89,6 +100,84 @@ function Dashboard({ data }) {
                 ],
               }}
             />
+          </Card>
+        </div>
+      </div>
+      <div
+        style={{
+          marginTop: "10vh",
+          marginLeft: "8vw",
+          display: "flex",
+          justifyContent: "space-between",
+          marginRight: "8vw",
+        }}
+      >
+        <div>
+          <Card
+            style={{
+              margin: 0,
+              padding: 20,
+              width: "38vw",
+              minHeight: "30vh",
+              maxHeight: "60vh",
+              // padding: 20,
+              backgroundColor: " #CCCCFF",
+              overflowY: "auto",
+              className: "style-2",
+              border: "2px solid green",
+              color: "black",
+              fontSize: "20px",
+              borderRadius: 5,
+            }}
+          >
+            <h2
+              style={{
+                fontFamily: "monospace",
+                marginBottom: "2vh",
+                color: "#008631",
+              }}
+            >
+              PROS:
+            </h2>
+            <ul>
+              {pro.map((item) => (
+                <li>item</li>
+              ))}
+            </ul>
+          </Card>
+        </div>
+        <div style={{ marginLeft: "11vw" }}>
+          <Card
+            style={{
+              margin: 0,
+              padding: 20,
+              width: "38vw",
+              minHeight: "30vh",
+              maxHeight: "60vh",
+              // padding: 20,
+              backgroundColor: " #CCCCFF",
+              overflowY: "auto",
+              className: "style-2",
+              border: "2px solid red",
+              color: "black",
+              fontSize: "20px",
+              borderRadius: 5,
+            }}
+          >
+            <h2
+              style={{
+                fontFamily: "monospace",
+                marginBottom: "2vh",
+                color: "red",
+              }}
+            >
+              CONS:
+            </h2>
+            <ul>
+              {con.map((item) => (
+                <li>item</li>
+              ))}
+            </ul>
           </Card>
         </div>
       </div>
